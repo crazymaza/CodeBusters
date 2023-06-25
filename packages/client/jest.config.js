8d0d0b98e33334b6a75fff52dd44de0cf412f14c
Igor <dx2389@gmail.com>
2023-06-25 19:53:18 +0300

    fix

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -10,10 +10,9 @@ export default {
     '^themes/(.*)$': '<rootDir>/src/themes/$1',
     '^sprites/(.*)$': '<rootDir>/src/assets/sprites/$1',
     '^images/(.*)$': '<rootDir>/src/assets/images/$1',
-    '^icons/(.*)$': '<rootDir>/src/assets/icons/$1',
     '^fonts/(.*)$': '<rootDir>/src/assets/fonts/$1',
     '^.+\\.(css|sass|scss)$': 'identity-obj-proxy',
-    '^icons/(.*)$': 'identity-obj-proxy'
+    '^icons/(.*)$': 'identity-obj-proxy',
   },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
