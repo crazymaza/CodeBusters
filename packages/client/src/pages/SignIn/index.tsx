77ab3f40233cc9a25a2b9e90554bb6f287810a25
Bulat <bulat40k@gmail.com>
2023-06-25 21:20:07 +0300

    auth logic in signin page

--- packages/client/src/pages/SignIn/index.tsx
+++ packages/client/src/pages/SignIn/index.tsx
@@ -1,28 +1,64 @@
-import { AuthLayout } from '@/layouts'
+import { useState } from 'react'
 import classNames from 'classnames/bind'
 import styles from './styles.module.scss'
 
 import { Link } from 'react-router-dom'
 import { Grid, Typography, TextField, Button } from '@mui/material'
+import { AuthLayout } from '@/layouts'
+import { useAppDispatch } from '@/store/typedHooks'
+import { useNavigate } from 'react-router-dom'
+import { getUserInfo, signin } from '@/store/slices/authSlice/thunks'
 
 const cx = classNames.bind(styles)
 
-// TODO: пока без логики
-
 const SignInPage = () => {
+  // TODO: пока так, иная реализация будет при подключении react-hook-form
+  const [login, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+
+  const submitLoginHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    dispatch(signin({ login, password }))
+      // .then(() => dispatch(getUserInfo()))
+      .then(() => navigate('/profile'))
+  }
+
+  const onChangeLoginHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value)
+  }
+
+  const onChangePasswordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
   return (
     <AuthLayout>
-      <Grid component="form" className={cx('signin')}>
+      <Grid
+        component="form"
+        className={cx('signin')}
+        onSubmit={submitLoginHandler}>
         <Typography variant="h3" component="h1" className={cx('signin__title')}>
           Вход
         </Typography>
         <Grid className={cx('signin__inputs')}>
-          <TextField label="Логин" variant="standard" />
-          <TextField label="Пароль" variant="standard" />
+          <TextField
+            label="Логин"
+            variant="standard"
+            onChange={onChangeLoginHandler}
+          />
+          <TextField
+            label="Пароль"
+            variant="standard"
+            onChange={onChangePasswordHandler}
+          />
         </Grid>
 
         <Link to={'/sign-up'}>Создать аккаунт</Link>
-        <Button variant="contained">Авторизоваться</Button>
+        <Button variant="contained" type="submit">
+          Авторизоваться
+        </Button>
       </Grid>
     </AuthLayout>
   )
