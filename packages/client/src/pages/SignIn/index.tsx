f369ff00a98e106aaea2a1c7de78df04be265575
Bulat <bulat40k@gmail.com>
2023-06-29 22:15:40 +0300

    add yup resolver, validate on signin page

--- packages/client/src/pages/SignIn/index.tsx
+++ packages/client/src/pages/SignIn/index.tsx
@@ -1,29 +1,65 @@
-import { useState } from 'react'
 import classNames from 'classnames/bind'
 import styles from './styles.module.scss'
 
 import { Link } from 'react-router-dom'
-import { Grid, Typography, TextField, Button } from '@mui/material'
+import {
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  FormHelperText,
+} from '@mui/material'
 import { AuthLayout } from '@/layouts'
 import { useAppDispatch } from '@/store/typedHooks'
 import { useNavigate } from 'react-router-dom'
 import { getUserInfo, signin } from '@/store/slices/authSlice/thunks'
 import { isAxiosError } from 'axios'
 
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm, Controller } from 'react-hook-form'
+import { SigninData } from '@/api/Auth/types'
+
 const cx = classNames.bind(styles)
 
+const schema = yup.object().shape({
+  login: yup
+    .string()
+    .required('Необходимое поле')
+    .matches(
+      /^(?=.*[a-zA-Z])([a-zA-Z0-9-_]){3,20}$/,
+      'от 3 до 20 символов, латиница, может содержать цифры, но не состоять из них, без пробелов, без спецсимволов (допустимы дефис и нижнее подчёркивание)'
+    ),
+  password: yup
+    .string()
+    .required('Необходимое поле')
+    .matches(
+      /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d].{8,40}$/,
+      'от 8 до 40 символов, обязательно хотя бы одна заглавная буква и цифра'
+    ),
+})
+
 const SignInPage = () => {
-  // TODO: пока так, иная реализация будет при подключении react-hook-form
-  const [login, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'all',
+  })
+
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
 
-  const submitLoginHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const onSubmit = async (data: SigninData) => {
+    const signInData = {
+      login: data.login,
+      password: data.password,
+    }
 
     try {
-      await dispatch(signin({ login, password })).unwrap()
+      await dispatch(signin(signInData)).unwrap()
       await dispatch(getUserInfo())
       navigate('/')
     } catch (error) {
@@ -33,34 +69,61 @@ const SignInPage = () => {
     }
   }
 
-  const onChangeLoginHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value)
-  }
-
-  const onChangePasswordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
-  }
-
   return (
     <AuthLayout>
       <Grid
         component="form"
         className={cx('signin')}
-        onSubmit={submitLoginHandler}>
+        onSubmit={handleSubmit(onSubmit)}>
         <Typography variant="h3" component="h1" className={cx('signin__title')}>
           Вход
         </Typography>
         <Grid className={cx('signin__inputs')}>
-          <TextField
-            label="Логин"
-            variant="standard"
-            onChange={onChangeLoginHandler}
+          <Controller
+            name="login"
+            control={control}
+            render={({ field: { onChange, ...props } }) => (
+              <TextField
+                {...props}
+                onChange={onChange}
+                onFocus={onChange}
+                variant="standard"
+                label="Логин"
+              />
+            )}
           />
-          <TextField
-            label="Пароль"
-            variant="standard"
-            onChange={onChangePasswordHandler}
+          {errors.login && (
+            <FormHelperText
+              sx={{ color: 'red' }}
+              required
+              id="login"
+              component="span">
+              {errors.login.message}
+            </FormHelperText>
+          )}
+
+          <Controller
+            name="password"
+            control={control}
+            render={({ field: { onChange, ...props } }) => (
+              <TextField
+                {...props}
+                onChange={onChange}
+                onFocus={onChange}
+                variant="standard"
+                label="Логин"
+              />
+            )}
           />
+          {errors.password && (
+            <FormHelperText
+              sx={{ color: 'red' }}
+              required
+              id="password"
+              component="span">
+              {errors.password.message}
+            </FormHelperText>
+          )}
         </Grid>
 
         <Link to={'/sign-up'}>Создать аккаунт</Link>
