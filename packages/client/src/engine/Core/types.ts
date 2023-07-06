52693a2e4f26c6a01274570735bb2c715d488eb5
alexYurin <sans.y@mail.ru>
2023-07-06 15:28:35 +0700

    added game process enum, some fixes

--- packages/client/src/engine/Core/types.ts
+++ packages/client/src/engine/Core/types.ts
@@ -3,3 +3,9 @@ import BaseObject, { BaseObjectSpecs } from '@/engine/Objects/Base'
 export type CBEngineOptions = {
   objects: BaseObject<BaseObjectSpecs>[]
 }
+
+export enum CBEngineProcess {
+  PLAY = 'play',
+  PAUSE = 'pause',
+  STOP = 'stop',
+}
