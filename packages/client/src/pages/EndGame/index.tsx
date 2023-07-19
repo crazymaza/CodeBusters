71dca55c3b98d2fffd76bc3899a8abb39b9045b7
yandexlispeth <alina_magic90@icloud.com>
2023-07-19 16:36:35 +0400

    Конфликты на странице EndGame

--- packages/client/src/pages/EndGame/index.tsx
+++ packages/client/src/pages/EndGame/index.tsx
@@ -1,11 +1,9 @@
 import { MainStage } from '@/components'
 import { MainLayout } from '@/layouts'
-import classNames from 'classnames/bind'
-import EndGameContent from './components/EndGameContent'
-import { useAppDispatch } from '@/store/typedHooks'
-import { logout } from '@/store/slices/userSlice/thunks'
 import { selectGameScores } from '@/store/slices/gameSlice/selectrors'
 import { useAppSelector } from '@/store/typedHooks'
+import classNames from 'classnames/bind'
+import EndGameContent from './components/EndGameContent'
 import styles from './styles.module.scss'
 
 const cx = classNames.bind(styles)
