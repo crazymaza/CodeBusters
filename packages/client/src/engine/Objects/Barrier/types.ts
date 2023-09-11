0b7fded0760b06ce2750df425d0360c0a2b39d8d
alexYurin <sans.y@mail.ru>
2023-09-11 19:35:04 +0700

    removed old files

--- packages/client/src/engine/Objects/Barrier/types.ts
+++
@@ -1,5 +0,0 @@
-import { BaseObjectSpecs } from '../Base/_types'
-
-export interface BarrierObjectSpecs extends BaseObjectSpecs {
-  image?: CanvasImageSource
-}
--- packages/client/src/engine/Objects/Base/_types.ts
+++
@@ -1,8 +0,0 @@
-export interface BaseObjectSpecs {
-  x: number
-  y: number
-  width: number
-  height: number
-  fill?: string
-  type?: string
-}
